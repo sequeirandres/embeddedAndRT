9fd68df68a9ac8502bb21145d0b7b167fb21782c
Marcelo Lorenzati <mlorenzati@gmail.com>
2024-05-10 21:16:56 -0300

    Added pico FreeRTOS examples

--- chapter3/src/FreeRTOSConfig.h
+++ chapter3/src/FreeRTOSConfig.h
@@ -1,16 +1,18 @@
-
 #ifndef FREERTOS_CONFIG_H
 #define FREERTOS_CONFIG_H
 
+/* Use Pico SDK ISR handlers */
+#define vPortSVCHandler         isr_svcall
+#define xPortPendSVHandler      isr_pendsv
+#define xPortSysTickHandler     isr_systick
 
 #define configUSE_PREEMPTION                    1
 #define configUSE_PORT_OPTIMISED_TASK_SELECTION 0
 #define configUSE_TICKLESS_IDLE                 0
-#define configCPU_CLOCK_HZ                      125000000/* Looking at runtime.c in the RPI 2040 SDK, the sys clock frequency is 125MHz */
-#define configSYSTICK_CLOCK_HZ                  1000000  /* This is always 1MHz on ARM I think.... */
-#define configTICK_RATE_HZ                      1000      /* I personally like 1kHz so you can do 1 ms sleeps */
+#define configCPU_CLOCK_HZ                      133000000
+#define configTICK_RATE_HZ                      100
 #define configMAX_PRIORITIES                    5
-#define configMINIMAL_STACK_SIZE                128      /* you might want to increase this, especially if you do any floating point printf  *YIKES* */
+#define configMINIMAL_STACK_SIZE                128
 #define configMAX_TASK_NAME_LEN                 16
 #define configUSE_16_BIT_TICKS                  0
 #define configIDLE_SHOULD_YIELD                 1
@@ -19,7 +21,6 @@
 #define configUSE_MUTEXES                       0
 #define configUSE_RECURSIVE_MUTEXES             0
 #define configUSE_COUNTING_SEMAPHORES           0
-#define configUSE_ALTERNATIVE_API               0 /* Deprecated! */
 #define configQUEUE_REGISTRY_SIZE               10
 #define configUSE_QUEUE_SETS                    0
 #define configUSE_TIME_SLICING                  0
@@ -28,14 +29,11 @@
 #define configNUM_THREAD_LOCAL_STORAGE_POINTERS 5
 #define configSTACK_DEPTH_TYPE                  uint16_t
 #define configMESSAGE_BUFFER_LENGTH_TYPE        size_t
-#define configENABLE_MPU                        0
-#define configSYSTEM_CALL_STACK_SIZE            128
 
 /* Memory allocation related definitions. */
-#define configSUPPORT_STATIC_ALLOCATION         1
+#define configSUPPORT_STATIC_ALLOCATION         0
 #define configSUPPORT_DYNAMIC_ALLOCATION        1
-#define configTOTAL_HEAP_SIZE                   50000
-#define configAPPLICATION_ALLOCATED_HEAP        0
+#define configAPPLICATION_ALLOCATED_HEAP        1
 
 /* Hook function related definitions. */
 #define configUSE_IDLE_HOOK                     0
@@ -59,20 +57,8 @@
 #define configTIMER_QUEUE_LENGTH                10
 #define configTIMER_TASK_STACK_DEPTH            configMINIMAL_STACK_SIZE
 
-/* Interrupt nesting behaviour configuration. */
-#define configKERNEL_INTERRUPT_PRIORITY         [dependent of processor]
-#define configMAX_SYSCALL_INTERRUPT_PRIORITY    [dependent on processor and application]
-#define configMAX_API_CALL_INTERRUPT_PRIORITY   [dependent on processor and application]
-
 /* Define to trap errors during development. */
-//#define configASSERT( ( x ) ) assert()
-
-/* FreeRTOS MPU specific definitions. */
-#define configINCLUDE_APPLICATION_DEFINED_PRIVILEGED_FUNCTIONS 0
-#define configTOTAL_MPU_REGIONS                                8 /* Default value. */
-#define configTEX_S_C_B_FLASH                                  0x07UL /* Default value. */
-#define configTEX_S_C_B_SRAM                                   0x07UL /* Default value. */
-#define configENFORCE_SYSTEM_CALLS_FROM_KERNEL_ONLY            1
+#define configASSERT( x )
 
 /* Optional functions - most linkers will remove unused functions anyway. */
 #define INCLUDE_vTaskPrioritySet                1
@@ -92,11 +78,6 @@
 #define INCLUDE_xTaskAbortDelay                 0
 #define INCLUDE_xTaskGetHandle                  0
 #define INCLUDE_xTaskResumeFromISR              1
-
-#define vPortSVCHandler isr_svcall
-#define xPortPendSVHandler isr_pendsv
-#define xPortSysTickHandler isr_systick
-
 
 /* A header file that defines trace macro can be included here. */
 
--- chapter3/src/main.c
+++ chapter3/src/main.c
@@ -2,35 +2,145 @@
 #include "pico/stdlib.h"
 #include "FreeRTOS.h"
 #include "task.h"
+#include "semphr.h"
 
+#if 1
+// Simple led task
 void vTaskCode( void * pvParameters )
 {
     /* The parameter value is expected to be 1 as 1 is passed in the
     pvParameters value in the call to xTaskCreate() below. 
     configASSERT( ( ( uint32_t ) pvParameters ) == 1 );
     */
-    for( ;; )
-    {
-            gpio_put(PICO_DEFAULT_LED_PIN, 1);
-            vTaskDelay(1000);
-            gpio_put(PICO_DEFAULT_LED_PIN, 0);
-            vTaskDelay(1000);
+    for( ;; ) {
+        gpio_put(PICO_DEFAULT_LED_PIN, 1);
+        vTaskDelay(pdMS_TO_TICKS(1000));
+        gpio_put(PICO_DEFAULT_LED_PIN, 0);
+        vTaskDelay(pdMS_TO_TICKS(1000));
     }
 }
 
 int main() {
     stdio_init_all();
-    
+
+#ifdef PICO_DEFAULT_LED_PIN
+    gpio_init(PICO_DEFAULT_LED_PIN);
+    gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
+#endif
+
     TaskHandle_t xHandle = NULL;
     /* Create the task, storing the handle. */
     BaseType_t xReturned = xTaskCreate(
-                    vTaskCode,       /* Function that implements the task. */
-                    "Blinky task",   /* Text name for the task. */
-                    512,             /* Stack size in words, not bytes. */
-                    ( void * ) 1,    /* Parameter passed into the task. */
-                    tskIDLE_PRIORITY,/* Priority at which the task is created. */
-                    &xHandle );   
+        vTaskCode,       /* Function that implements the task. */
+        "Blinky task",   /* Text name for the task. */
+        512,             /* Stack size in words, not bytes. */
+        ( void * ) 1,    /* Parameter passed into the task. */
+        tskIDLE_PRIORITY,/* Priority at which the task is created. */
+        &xHandle );   
 
     vTaskStartScheduler();
     __builtin_unreachable();
 }
+#endif
+
+#if 0
+// Two tasks with same code but different priorities
+void vTaskCode( void * pvParameters ) {
+    int taskId = *((int *)pvParameters);
+    for( ;; ) {
+        printf("Task %d task\n", taskId);
+        vTaskDelay(pdMS_TO_TICKS(1000));
+    }
+}
+
+int main() {
+    stdio_init_all();
+  
+    TaskHandle_t xHandle1 = NULL;
+    TaskHandle_t xHandle2 = NULL;
+    /* Create 2 task */
+    int tid1 = 1, tid2 = 2;
+    BaseType_t xReturned1 = xTaskCreate( vTaskCode, "talking task1", 512, ( void * ) &tid1, 1, &xHandle1 );  
+    BaseType_t xReturned2 = xTaskCreate( vTaskCode, "talking task2", 512, ( void * ) &tid2, 1, &xHandle2 );
+    vTaskStartScheduler();
+    __builtin_unreachable();
+}
+#endif
+
+#if 0
+// Two tasks with different yielding
+void vTaskYielding( void * pvParameters ) {
+    int taskId = *((int *)pvParameters);
+    for( ;; ) {
+        printf("doing something and sleeping ID %d\n", taskId);
+        vTaskDelay(pdMS_TO_TICKS(1000));
+        printf("Doing more and yielding ID %d\n", taskId);
+        taskYIELD();
+    }
+}
+
+void vTaskUnfriendly( void * pvParameters ) {
+    int taskId = *((int *)pvParameters);
+    for( ;; ) {
+        printf("doing something and busy waiting ID %d\n", taskId);
+        uint64_t start_ticks = time_us_64();
+        uint64_t end_ticks = start_ticks + 2000000;
+        while (time_us_64() < end_ticks) {
+            tight_loop_contents();
+        }
+    }
+}
+
+int main() {
+    stdio_init_all();
+  
+    TaskHandle_t xHandle1 = NULL;
+    TaskHandle_t xHandle2 = NULL;
+     /* Create 2 task */
+    int tid1 = 1, tid2 = 2;
+    BaseType_t xReturned1 = xTaskCreate( vTaskYielding, "Yielding Task", 512, ( void * ) &tid1, 2, &xHandle1 );  
+    BaseType_t xReturned2 = xTaskCreate( vTaskUnfriendly, "Unfriendly Task", 512, ( void * ) &tid2, 1, &xHandle2 );
+    vTaskStartScheduler();
+    __builtin_unreachable();
+}
+#endif
+
+#if 0
+SemaphoreHandle_t xSemaphore;
+
+// Two tasks sharing resources
+void vTaskSem( void * pvParameters ) {
+    int taskId = *((int *)pvParameters);
+    for( ;; ) {
+        printf("Running Task ID %d\n", taskId);
+        if (xSemaphoreTake(xSemaphore, pdMS_TO_TICKS(100) ) != pdTRUE) {
+            printf("Task %d failed to capture semaphore\n", taskId);
+        } else {
+            printf("Task %d doing some job\n", taskId);
+            vTaskDelay(pdMS_TO_TICKS(10));
+
+            printf("Task %d returning semaphore\n", taskId);
+            xSemaphoreGive(xSemaphore);
+        }
+
+        vTaskDelay(pdMS_TO_TICKS(1000));
+    }
+}
+
+int main() {
+    stdio_init_all();
+  
+    TaskHandle_t xHandle1 = NULL;
+    TaskHandle_t xHandle2 = NULL;
+
+    vSemaphoreCreateBinary( xSemaphore );
+    printf("Semaphore creation was %s\n", xSemaphore == NULL ? "failed": "succesful");
+
+     /* Create 2 task */
+     int tid1 = 1, tid2 = 2;
+    BaseType_t xReturned1 = xTaskCreate( vTaskSem, "Yielding Task 1", 512, ( void * ) &tid1, 1, &xHandle1 );
+    BaseType_t xReturned2 = xTaskCreate( vTaskSem, "Yielding Task 2", 512, ( void * ) &tid2, 1, &xHandle2 );  
+    vTaskStartScheduler();
+    __builtin_unreachable();
+}
+#endif
